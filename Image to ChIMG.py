9cf6442750f424376aa242cb7b2e91982a17a6c2
Charlo Brown <116456445+charlobrown@users.noreply.github.com>
2025-10-13 21:38:59 -0700

    MIT License

--- Image to ChIMG.py
+++ Image to ChIMG.py
@@ -1,3 +1,13 @@
+"""
+Copyright 2025 Charlo Brown
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 #---------
 # Imports |
 #---------
@@ -107,3 +117,4 @@ compImgData = compressors[encoder](imgData)
 finalImageData = f"ChImg;;{image.mode};;{image.img.size};;{encoder}:;".encode("UTF-8")
 
 open(os.path.join(os.path.dirname(imageFile), (os.path.basename(imageFile) + ".ChImg")), "wb").write(finalImageData + compImgData) # Writes the data to a file.
+
